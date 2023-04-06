b46779a516e3d3c3b8ab6ee7eb272d255e583970
THIAGO VIUDES CARSADO <129440601+viudes-13@users.noreply.github.com>
2023-04-06 14:48:11 -0300

    exercio do objeto

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -17,26 +17,55 @@ console.log (livro.autor);
 
 console.log('-------------');
 
-//
+
 
 let pessoa = {
     nome: 'Ozzy Osbourne',
     idade: 70,
-    telefones: ['11-2134-0300'],
-    endereço {
+    telefones: ['11-2134-0300', '11-98888-0000'],
+    endereço: {
         logradouro:  'Rua XYZ',
-        numero:  '211' ,
+        numero: '211' ,
         bairro: 'Guapira',
         cidade: 'São Paulo'
     }
-    
-
 };
 
-let livros = [
-    {
-        titulo: 'inside out'
-        autor: 'joao'
-        ano: '1910'
-    }
-]
+    
+
+
+
+
+console.log ('------------')
+
+
+
+let pedido = {
+    codigo: "123abcd",
+    produtos: ['smartphone', 'teclado', 'mouse'],
+    data: "06/04/2023",
+    preco: 4000.95,
+    prazo: 7
+}
+
+
+
+
+console.log(`referente ao pedido ${pedido.codigo}, os produtos foram comprados em ${pedido.data} custando  ${pedido.preco}.
+prazo de entrega é  ${pedido.prazo} dias úteis  `)
+
+
+
+//crie um objeto chamado "pedido" contendo as seguintes irfomaçoes
+
+//codigo (invente uma combinação de letras e numeros)
+//lista de (3 produtos basta o nome de cada um)
+//data do pedido (colque a data de hoje)
+//preço
+//prazo de entrega em dias
+
+//Mostre no console uma frase contendo algumas das
+//informaçoes do pedido. 
+//exemplo: referente ao pedido 123xyz, os produtos foram comprados
+//em 06/04/2023 custando 2023 custando 4500. o prazo de entrega é de 7 dias uteis
+
